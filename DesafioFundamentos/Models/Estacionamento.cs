ed76d8924382dbf71aa9ceb65c543f24612544e8
luisasza <luisasza@outlook.com>
2023-12-27 14:54:22 -0300

    tratamento de exceção

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,54 +16,83 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
+             try
+             {
+                // Pede para o usuário digitar a placa e armazena na variável placa
+                Console.WriteLine("Digite a placa do veículo para estacionar:");
+                string placa = Console.ReadLine();
+                placa = placa.ToUpper();
+
+                // Checa se a placa já está na lista veiculos
+                if (veiculos.Contains(placa))
+                {
+                    Console.WriteLine("Veículo já cadastrado. Não é possível cadastrar novamente.");
+                }
+                else
+                {
+                     //cadastra o novo veículo na lista veiculos
+                     veiculos.Add(placa);
+                    // guarda a data em que foi feita a operação
+                    DateTime dataInclusao = DateTime.Now;
+
+                    // declara a placa recém cadastrada, data e horário de cadastro
+                    Console.WriteLine($"Veículo {placa} cadastrado às {dataInclusao.ToString("dd/MM/yyyy HH:mm")}");
+                }
+             }
+
+            // Trata exceção
+            catch (Exception ex)
+             {
+                Console.WriteLine($"Ocorreu um erro. {ex.Message}");
+             }
+
             
-            // Pede para o usuário digitar a placa e armazena na variável placa
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
-            string placa = Console.ReadLine();
-
-            //cadastra o novo veículo na lista veiculos
-            veiculos.Add(placa);
-
-            // guarda a data em que foi feita a operação
-            DateTime dataInclusao = DateTime.Now;
-
-            // declara a placa recém cadastrada, data e horário de cadastro
-            Console.WriteLine($"Veículo {placa.ToUpper()} cadastrado às {dataInclusao.ToString("dd/MM/yyyy HH:mm")}");
         }   
 
         public void RemoverVeiculo()
         {
-            //Pede para o usuário digitar a placa e armazena na variável placa
-            Console.WriteLine("Digite a placa do veículo a ser removido:");
-            string placa = Console.ReadLine();
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            try
             {
-                 int horas = 0;
+             
+                //Pede para o usuário digitar a placa e armazena na variável placa
+                Console.WriteLine("Digite a placa do veículo a ser removido:");
+                string placa = Console.ReadLine();
+                placa = placa.ToUpper();
+                int horas = 0;
+            
 
-                 // guarda a data em que foi feita a operação
-                 DateTime dataRemocao = DateTime.Now;
+                // Verifica se o veículo existe
+                if (veiculos.Contains(placa))
+                {
+                
+                    // guarda a data em que foi feita a operação
+                    DateTime dataRemocao = DateTime.Now;
 
-                // Pede para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado e atualiza a variável horas com o valor
-                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-                 horas = int.Parse(Console.ReadLine());
+                    // Pede para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado e atualiza a variável horas com o valor
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                    horas = int.Parse(Console.ReadLine());
 
-                // Calcula o valor total a ser pago
-                 decimal valorTotal = precoInicial + (precoPorHora * horas); 
+                    // Calcula o valor total a ser pago
+                    decimal valorTotal = precoInicial + (precoPorHora * horas); 
 
-                // Remover a placa digitada da lista de veículos e guarda na variável veiculoRemovido
-                 string veiculoRemovido = placa;
-                 veiculos.Remove(placa);
+                    // Remover a placa digitada da lista de veículos e guarda na variável veiculoRemovido
+                     veiculos.Remove(placa);
 
                 
-
-                 Console.WriteLine($"O veículo de placa {placa.ToUpper()} foi removido às {dataRemocao.ToString("HH:mm")} de {dataRemocao.ToString("dd/MM/yyyy")}.");
-                 Console.WriteLine($"O preço total a ser pago é de:  {valorTotal:C}");
+                    // Mostra no console o resultado da remoção
+                    Console.WriteLine($"O veículo de placa {placa.ToUpper()} foi removido às {dataRemocao.ToString("HH:mm")} de {dataRemocao.ToString("dd/MM/yyyy")}.");
+                    Console.WriteLine($"O preço total a ser pago é de:  {valorTotal:C}");
+                }
+                else
+                {
+                    Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                }
             }
-            else
+
+            // Trata exceção
+            catch (Exception ex)
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine($"Ocorreu um erro. {ex.Message}");
             }
         }
 
@@ -73,8 +102,7 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                  Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+               // Exibe todos os veículos cadastrados
                  foreach (var carro in veiculos)
                  {
                     Console.WriteLine(carro.ToUpper());
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -7,12 +7,20 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+try
+{
+    Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+    precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Agora digite o preço por hora:");
+    precoPorHora = Convert.ToDecimal(Console.ReadLine());
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu um erro. {ex.Message}");
+}
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
@@ -53,7 +61,7 @@ while (exibirMenu)
             break;
     }
 
-    Console.WriteLine("Pressione uma tecla para voltar ao menu");
+    Console.WriteLine("Pressione uma tecla para prosseguir");
     Console.ReadLine();
 }
 
